8292d3807a8dd1e9739fb595f2e42689160b3e4e
JSCatLower <kalmykovyaroslavkalmykov@yandex.ru>
2025-08-16 23:07:42 +0400

    Добавил фичи C++23!

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "math.hpp"
 #include "cat.hpp"
+#include <print>
 
 int main() {
   std::cout << "Привет из C++!" << std::endl;
   std::cout << "Кстати, 2 + 3 = " << add(2, 3) << std::endl;
   std::cout << "10 / 3 = " << divide(10, 3) << std::endl;
+  std::print("Раз раз..😌🐱\n");
   Cat sigma("sigma😎");
   sigma.meow();
 }
