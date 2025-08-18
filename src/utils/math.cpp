eba0b941093fe28685c7d2810d8e1ca1509aba13
JSCatLower <kalmykovyaroslavkalmykov@yandex.ru>
2025-08-18 11:47:11 +0000

    Переделал math.cpp

--- src/utils/math.cpp
+++ src/utils/math.cpp
@@ -6,12 +6,12 @@ double add(double a, double b) {
 double sub(double a, double b) {
     return a - b;
 }
-double mult(double a, dodouble) {
+double mult(double a, double b) {
     return a * b;
 }
 double divide(double a, double b) {
     return a / b;
 }
 double square(double x) {
-    return x * x
+    return x * x;
 }
