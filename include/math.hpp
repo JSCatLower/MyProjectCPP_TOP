cca0d7e2564218ce62a5ec8c7a2eea13a30dcd82
JSCatLower <kalmykovyaroslavkalmykov@yandex.ru>
2025-08-18 11:43:45 +0000

    Добавит степень в квадрате

--- include/math.hpp
+++ include/math.hpp
@@ -4,3 +4,4 @@ double add(double a, double b);  // Объявление функции
 double sub(double a, double b);
 double mult(double a, double b);
 double divide(double a, double b);
+double square(double x);
--- src/utils/math.cpp
+++ src/utils/math.cpp
@@ -6,9 +6,12 @@ double add(double a, double b) {
 double sub(double a, double b) {
     return a - b;
 }
-double mult(double a, double b) {
+double mult(double a, dodouble) {
     return a * b;
 }
 double divide(double a, double b) {
     return a / b;
+}
+double square(double x) {
+    return x * x
 }
