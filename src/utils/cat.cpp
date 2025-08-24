5ac25d4b5ea1381f26b4a83b22084cf04087f183
JSCatLower <kalmykovyaroslavkalmykov@yandex.ru>
2025-08-24 14:02:41 +0400

    Добавил сообщение о создании котика

--- src/utils/cat.cpp
+++ src/utils/cat.cpp
@@ -2,7 +2,9 @@
 #include "cat.hpp"  // Подключаем свой заголовочный файл
 #include <iostream>
 
-Cat::Cat(const std::string& name) : name(name) {}
+Cat::Cat(const std::string& name) : name(name) {
+    std::cout << "Создался котик под именем " << name << std::endl;
+}
 
 void Cat::meow() const {
     std::cout << name << " Говорит: Мяу!" << "\n";
